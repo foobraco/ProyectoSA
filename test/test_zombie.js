48395d9e08616fa94ff59c8d85d81163e4859986
foobraco <bryan.alvarado@gmail.com>
2013-11-16 08:56:17 -0600

    Added more routes testing

--- test/test_zombie.js
+++ test/test_zombie.js
@@ -4,24 +4,15 @@ var assert = require('assert')
   , zombie = require('zombie')
   ;
 
-describe('front page', function() {
-  it('should load', function () {
-    var browser = new zombie();
-    browser.visit("http://localhost:3000/", function () {
-      assert.ok(browser.success);
-      if (browser.error) {
-        console.dir("errors reported:", browser.errors);
-      }
-    });
-  });
+// describe('front page', function() {
+//   it('should load', function () {
+//     var browser = new zombie();
+//     browser.visit("http://localhost:4000/", function () {
+//       assert.ok(browser.success);
+//       if (browser.error) {
+//         console.dir("errors reported:", browser.errors);
+//       }
+//     });
+//   });
   
-  it('should have the page title Liatorp', function() {
-    var browser = new zombie();
-    browser.visit("http://localhost:3000/", function () {
-      assert.equal(browser.text("title"), "Liatorp");      
-      if (browser.error) {
-        console.dir("errors reported:", browser.errors);
-      }
-    });
-  });
-});
+// });
